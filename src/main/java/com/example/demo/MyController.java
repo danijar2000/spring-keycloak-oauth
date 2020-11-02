305e9d9712d5fd27e0dc6f5c23a5432f80c20b3f
doni <doni2000@mail.ru>
2020-11-03 02:20:28 +0600

    my page

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -2,16 +2,26 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.representations.AccessToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.ServletException;
-import java.util.Arrays;
+import java.security.Principal;
 
 @Controller
 class ProductController {
 
     @GetMapping(path = "/my")
-    public String getProducts(){
+    public String myPage(Model model) {
+        AccessToken token = getKeycloakSecurityContext().getToken();
+        model.addAttribute("first_name", token.getGivenName());
+        model.addAttribute("last_name", token.getFamilyName());
         return "my";
     }
 
@@ -19,5 +29,29 @@ class ProductController {
     public String logout(HttpServletRequest request) throws ServletException {
         request.logout();
         return "redirect:/";
+    }
+
+    protected KeycloakSecurityContext getKeycloakSecurityContext() {
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes)
+                RequestContextHolder.getRequestAttributes(); Principal principal = attributes.getRequest().getUserPrincipal();
+
+        if (principal instanceof KeycloakPrincipal)
+        {
+            KeycloakSecurityContext koj = KeycloakPrincipal.class.cast(principal).getKeycloakSecurityContext();
+
+            return KeycloakPrincipal.class.cast(principal).getKeycloakSecurityContext();
+        }
+
+        return null;
+    }
+
+    public String getUserGroup() {
+        return getKeycloakSecurityContext().getToken().getOtherClaims().get("group").
+                toString();
+    }
+
+    public String getUserName() {
+        return getKeycloakSecurityContext().getToken().getName();
     }
 }
